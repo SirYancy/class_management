5f04e3e1219ccb220b68467307b806965c9a8c51
Eric Kuha <erickuha@gmail.com>
2016-08-24 22:33:51 -0500

    Initial Deploy. No functionality

--- Attendance/settings.py
+++ Attendance/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g2$6xt^=)6or1e#u#ywi_cw_y57&g%!@&ino7r-t*7c)3tpo0c'
+# SECRET_KEY = 'apple'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
