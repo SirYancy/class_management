b2ac46559977bda50f68d3fa4b89495175124173
Eric Kuha <erickuha@gmail.com>
2016-08-24 23:27:32 -0500

    Again fixed static files?

--- Attendance/settings.py
+++ Attendance/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static')
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # Simplified static file serving.
